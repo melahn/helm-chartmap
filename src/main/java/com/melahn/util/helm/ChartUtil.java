aae2c32c1d0c4a0c3b118536eed045b39b83c033
melahn <gregorymelahn@gmail.com>
2021-03-30 16:44:21 -0400

    reduce cognitive complexity to resolve sonar error

--- src/main/java/com/melahn/util/helm/ChartUtil.java
+++ src/main/java/com/melahn/util/helm/ChartUtil.java
@@ -2,8 +2,15 @@ package com.melahn.util.helm;
 
 import java.util.Map;
 
-public class ChartUtil {
+public final class ChartUtil {
 
+    /**
+     * Not called
+     */
+    private ChartUtil() {
+        
+    }
+     
     /**
      * Recursively searches the values map and finds a value for a given key
      *
@@ -13,25 +20,53 @@ public class ChartUtil {
      */
     @SuppressWarnings("unchecked") public static Object getValue(String k, Map<String, Object> values) {
         if (values != null) {
-            String head = k;
-            String tail = "";
-            if ((k.contains("."))) {
-                head = k.substring(0, k.indexOf('.')); // pop the first segment off the key string
-                tail = k.substring(k.indexOf('.') + 1, k.length());
-            }
-            if (!head.isEmpty()) {
-                for (Map.Entry<String, Object> entry : values.entrySet()) {
-                    if (head.equals(entry.getKey())) {
-                        if (!(entry.getValue() instanceof Map)) {    // if this is a leaf
-                            return entry.getValue(); // end the recursion
-                        }
-                        else if (!tail.isEmpty() && entry.getValue() instanceof Map) {
-                            return getValue(tail, (Map<String, Object>) entry.getValue()); // continue the recursion
-                        }
+            String head = getLead(k);
+            String tail = getTail(k);
+        if (!head.isEmpty()) {
+            for (Map.Entry<String, Object> entry : values.entrySet()) {
+                if (head.equals(entry.getKey()) && !(entry.getValue() instanceof Map)) {
+                        return entry.getValue(); // end the recursion
+                    }
+                    else if ((head.equals(entry.getKey()) && !tail.isEmpty() && entry.getValue() instanceof Map)) {
+                        return getValue(tail, (Map<String, Object>) entry.getValue()); // continue the recursion
                     }
                 }
             }
         }
         return null;
+    }
+
+    /** 
+     * Extracts the leading portion of a string
+     * 
+     * @param k the string from which the extraction should occur
+     * @return returns a String with the leading segment of the string up but
+     * not including the dot.  If the string does not contains a dot returns the
+     * input string.
+     */
+    private static String getLead(String k) {
+        if ((k.contains("."))) {
+            return k.substring(0, k.indexOf('.')); // pop the first segment off the key string
+        }
+        else {
+            return k;
+        }
+    }
+
+     /** 
+     * Extracts the tail portion of a string
+     * 
+     * @param k the string from which the extraction should occur
+     * @return returns a String with the trailing segment of the string starting with 
+     * the position just past the dot.  If the string does not contains a dot returns the
+     * empty string.
+     */
+    private static String getTail(String k) {
+        if ((k.contains("."))) {
+            return k.substring(k.indexOf('.') + 1, k.length()); // pop the last segment off the key string
+        }
+        else {
+            return "";
+        }
     }
 }
