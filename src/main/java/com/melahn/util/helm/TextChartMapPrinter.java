026f64c54c6f5e36d9c762aa3e45fab4dd83176b
melahn <gregorymelahn@gmail.com>
2021-03-31 15:43:28 -0400

    winkling out sonar issues

--- src/main/java/com/melahn/util/helm/TextChartMapPrinter.java
+++ src/main/java/com/melahn/util/helm/TextChartMapPrinter.java
@@ -5,7 +5,7 @@ import org.apache.commons.collections4.map.MultiKeyMap;
 import com.melahn.util.helm.model.HelmChart;
 public class TextChartMapPrinter extends ChartMapPrinter {
 
-    public TextChartMapPrinter(ChartMap chartMap, String outputFilename, MultiKeyMap charts, HelmChart chart) {
+    public TextChartMapPrinter(ChartMap chartMap, String outputFilename, MultiKeyMap<String,String> charts, HelmChart chart) {
         super(chartMap, outputFilename, charts, chart);
     }
 }
